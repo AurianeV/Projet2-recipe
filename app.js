1bd5b30d67d6d4c0c5806670b0ff267eb27f161e
frenchToast <juliekogan1@gmail.com>
2021-12-04 15:57:46 +0100

    still wip

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 const express = require("express");
 
@@ -7,28 +7,25 @@ const hbs = require("hbs");
 
 const app = express();
 
-
 //server start
 app.listen(3000, () => console.log("my first app listening on port 3000!"));
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-    src:  path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    sourceMap: true
-  }));
-        
-  
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'hbs');
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true,
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
 
 // start route
 const index = require("./routes/index");
 app.use("/", index);
-
-
-
 
 module.exports = app;
