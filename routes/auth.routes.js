c58f6a7b2f0c85ee6ee59c26b51077f44723f723
Auriane Viggiano <auriane.vigg@gmail.com>
2021-12-15 18:28:47 +0100

    login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -46,7 +46,7 @@ router.post("/auth/signup", (req, res, next) => {
       });
 
       // .post() login route ==> to process form data
-      router.post('/auth/login', (req, res, next) => {
+      router.post('/login', (req, res, next) => {
         const { mail, password } = req.body;
       
         // Si l'utilisateur ne rempli pas les données --> message d'erreur
