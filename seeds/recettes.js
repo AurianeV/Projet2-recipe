2c709786d943ca8cddd564281bd49bf7ced20c4d
Auriane Viggiano <auriane.vigg@gmail.com>
2021-12-16 21:10:35 +0100

    filtre recette

--- seeds/recettes.js
+++ seeds/recettes.js
@@ -1,9 +1,27 @@
-const recette = [{
-    name:"",
-    saison:"",
-    duration:"",
+const recettes = [
+    
+    {
+        name:"Soupe au curry",
+        season: ["winter",  "autumn"],
+        duration:"2",
+        mood: ["comfort", "healthy"],
+        type: "diner",
+        difficulty:"easy",
+        ingredient:["curry",],
+    },
 
-},
+    {
+        name:"Soupe au curry",
+        season: ["winter",  "autumn"],
+        duration:"2",
+        mood: ["comfort", "healthy"],
+        type: "diner",
+        difficulty:"easy",
+        ingredient:["curry",],
+        },
+
+        
+
 ];
 
 
