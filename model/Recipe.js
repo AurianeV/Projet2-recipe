619c1bb226195bb4e139f69f9b2db47dd55da1f7
Auriane Viggiano <auriane.vigg@gmail.com>
2021-12-27 23:45:03 +0100

    image+page details

--- model/Recipe.js
+++ model/Recipe.js
@@ -4,17 +4,23 @@ const recipe = new Schema(
   {
     name: String,
     description: String,
-    Url: { type: String, default: '/public/images/fav.png' },
+    image: String,
+    time: String,
     season: [{ type: String, enum: ["winter", "spring", "autumn", "summer"] } ],
     duration: { type: String, enum: ['1', '2', '3'] },
     mood: [{ type: String, enum: ["comfort", "energy", "healthy"] } ],
     type: [{ type: String, enum: ["apero", "brunch", "petit-dejeuner", "diner" , "dejeuner", "dessert"] } ],
     difficulty: { type: String, enum: ['easy', 'medium', 'hard'] },
     ingredient: [String],
+    personne: String,
+    ingredients: String,
+    Étapes: [String],
   },
+  
   {
     timestamps: true,
   }
+
 );
 
 module.exports = model("Recipe", recipe);
