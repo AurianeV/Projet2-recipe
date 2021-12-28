c9b9a2d3a36cac9de8cb45767346008f2e22df4c
frenchToast <juliekogan1@gmail.com>
2021-12-28 03:59:54 +0100

    Images-description-etapes

--- model/Recipe.js
+++ model/Recipe.js
@@ -3,13 +3,25 @@ const { Schema, model } = require("mongoose");
 const recipe = new Schema(
   {
     name: String,
-    description: String,
-    Url: { type: String, default: '/public/images/fav.png' },
-    season: [{ type: String, enum: ["winter", "spring", "autumn", "summer"] } ],
-    duration: { type: String, enum: ['1', '2', '3'] },
-    mood: [{ type: String, enum: ["comfort", "energy", "healthy"] } ],
-    type: [{ type: String, enum: ["apero", "brunch", "petit-dejeuner", "diner" , "dejeuner", "dessert"] } ],
-    difficulty: { type: String, enum: ['easy', 'medium', 'hard'] },
+    description: [String],
+    Url: { type: String, default: "/public/images/fav.png" },
+    season: [{ type: String, enum: ["winter", "spring", "autumn", "summer"] }],
+    duration: { type: String, enum: ["1", "2", "3"] },
+    mood: [{ type: String, enum: ["comfort", "energy", "healthy"] }],
+    type: [
+      {
+        type: String,
+        enum: [
+          "apero",
+          "brunch",
+          "petit-dejeuner",
+          "diner",
+          "dejeuner",
+          "dessert",
+        ],
+      },
+    ],
+    difficulty: { type: String, enum: ["easy", "medium", "hard"] },
     ingredient: [String],
   },
   {
